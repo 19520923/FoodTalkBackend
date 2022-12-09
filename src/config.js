1eab0fd90ef1af7195ad3b0d5c2ec16480642488
TanNN <19520923@gm.uit.edu.vn>
2022-12-09 23:20:37 +0700

    update email

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ const config = {
     port: process.env.PORT || 9000,
     ip: process.env.IP || '0.0.0.0',
     apiRoot: process.env.API_ROOT || '',
-    defaultEmail: '19520923@gm.uit.edu.vn',
+    defaultEmail: 'nguyennhuttan.personal@gmail.com',
     sendgridKey: requireProcessEnv('SENDGRID_KEY'),
     masterKey: requireProcessEnv('MASTER_KEY'),
     jwtSecret: requireProcessEnv('JWT_SECRET'),
